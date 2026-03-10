84279d944e08e8e38d35b07e5bc1b1338b8ad1c9
vanmuoi24 <domuoigghh@gmail.com>
2026-03-10 18:22:48 +0700

    fix spa routing

--- ccid/vite.config.js
+++ ccid/vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [
-    react(),
-   viteStaticCopy({
-  targets: [
-    {
-      src: "public/_redirects",
-      dest: ".",
-    },
-  ],
-})
-  ],
+  plugins: [react()],
   build: {
     outDir: "dist",
   },
